421ee896ab726650a48344dd03e07a9a7608e626
actions-update-go <noreply@github.com>
2021-02-12 15:35:30 +0000

    https://github.com/Azure/azure-storage-azcopy/archive/v#{version}.tar.gz@v10.8.0

--- demo/azcopy/formula.rb
+++ demo/azcopy/formula.rb
@@ -1,8 +1,8 @@
 class AzCopy < DebianFormula
   homepage 'https://github.com/Azure/azure-storage-azcopy/'
-  version '10.7.0'
+  version '10.8.0'
   url "https://github.com/Azure/azure-storage-azcopy/archive/v#{version}.tar.gz"
-  sha256 'cfdc53dd2c5d30adddeb5270310ff566b4417a9f5eec6c9f6dfbe10d1feb6213'
+  sha256 'd5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed'
 
   name 'azcopy'
   arch 'x86_64'
