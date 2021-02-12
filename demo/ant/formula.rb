f233ecab83bb11bb953764037185820e52450bf4
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2021-02-12 10:24:46 -0500

    demo ant (smaller binary)

---
+++ demo/ant/formula.rb
@@ -0,0 +1,9 @@
+class Ant < DebianFormula
+  homepage 'http://ant.apache.org/'
+  url 'https://archive.apache.org/dist/ant/binaries/apache-ant-1.9.2-bin.tar.bz2'
+  sha1 '50cfaaeecee4f88a3ff9de5068fc98e4e9268daf'
+
+  name 'ant'
+  description 'ant java build tool'
+  version '1.9.2+thepwagner1'
+end
--- demo/hadoop/formula.rb
+++
@@ -1,9 +0,0 @@
-class Hadoop2 < DebianFormula
-  homepage 'http://hadoop.apache.org/'
-  url 'https://archive.apache.org/dist/hadoop/core/hadoop-2.6.0/hadoop-2.6.0.tar.gz'
-  sha1 '5b5fb72445d2e964acaa62c60307168c009d57c5'
-
-  name 'hadoop2'
-  description 'distributed computing and storage framework'
-  version '2.6.0+thepwagner1'
-end
